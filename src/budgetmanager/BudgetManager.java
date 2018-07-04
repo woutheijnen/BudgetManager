bda2ac483c850e0d535b0805c49b4e01fb3c1b28
wouth <wouth@DESKTOP-T5ASC68.home>
2018-07-05 01:25:10 +0200

    Initial commit: project files

---
+++ src/budgetmanager/BudgetManager.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package budgetmanager;
+
+/**
+ *
+ * @author Wout
+ */
+public class BudgetManager {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ManagerInterFace m = new ManagerInterFace();
+        m.setVisible(true);
+    }
+    
+}
